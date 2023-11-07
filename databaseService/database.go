a4bdec8933567b9fea79f1adef8e97d21ac4469c
RealCommonCat <128367240+RealCommonCat@users.noreply.github.com>
2023-11-07 11:50:07 +0800

    更新了返回gorm.DB进行复杂SQL操作的方法

--- databaseService/database.go
+++ databaseService/database.go
@@ -1,5 +1,7 @@
 package databaseService
 
+import "gorm.io/gorm"
+
 // CreateTable 创建表单
 // 传入：表单名称，表单结构体
 // 传出：错误消息
@@ -32,10 +34,17 @@ func (db *DatabaseAPP) UpdateData(tableName string, data interface{}) error {
 	return err
 }
 
-// GetData 获取数据
+// GetData 获取一个表单全部数据
 // 传入：表单名称，数据(传递指针)
 // 传出：错误消息
-func (db *DatabaseAPP) GetData(tableName string, data interface{}, conds interface{}) error {
-	err := db.db.Table(tableName).Find(data, conds).Error
+func (db *DatabaseAPP) GetData(tableName string, data interface{}) error {
+	err := db.db.Table(tableName).Find(data).Error
 	return err
+}
+
+// GetDatabase 获取数据库 进行复杂SQL操作
+// 传入：无
+// 传出：数据库
+func (db *DatabaseAPP) GetDatabase() *gorm.DB {
+	return db.db
 }
